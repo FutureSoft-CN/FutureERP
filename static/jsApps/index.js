801cdbf36cbe61ccdec75367cb46c9f421ad7d54
Tianwei Yang <yangtianwei@outlook.com>
2018-12-15 20:11:25 +0800

    Implement subpage dispatch
    Signed-off-by: Tianwei Yang <yangtianwei@outlook.com>

--- static/jsApps/index.js
+++ static/jsApps/index.js
@@ -1,10 +1,6 @@
-loadSubPage('homepage.html');
+loadSubPage('dashboard.html');
 
 function loadSubPage(url) {
 	$("#page-wrapper").load(staticUrl + "subPages/" + url);
-	// Morris Charts JavaScript
-	$("body").append('<script src="' + staticUrl + 'vendor/raphael/raphael.min.js"></script>');
-	$("body").append('<script src="' + staticUrl + 'vendor/morrisjs/morris.min.js"></script>');
-	$("body").append('<script src="' + staticUrl + 'data/morris-data.js"></script>');
 }
 
