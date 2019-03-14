cb7f7171bdd4c8634a9141a57ffd76b94c4997a6
Tianwei Yang <yangtianwei@outlook.com>
2019-03-14 23:16:04 +0800

    质检查询beta
    Signed-off-by: Tianwei Yang <yangtianwei@outlook.com>

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def load_dashboard(request):   
-    return render(request,'dashboard/index.html')
+    return render(request,'dashboard.html')
     
     
